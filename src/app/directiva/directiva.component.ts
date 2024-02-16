0f891efd756500dd7852158d4e100b99665f9365
Dmzeta <98977215+Dmzeta@users.noreply.github.com>
2024-02-16 18:05:41 +0100

    ng for y ng if directiva

--- src/app/directiva/directiva.component.ts
+++ src/app/directiva/directiva.component.ts
@@ -1,14 +1,20 @@
 import { Component } from '@angular/core';
-import {NgForOf} from "@angular/common";
+import {NgForOf, NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-directiva',
   standalone: true,
   imports: [
-    NgForOf
+    NgForOf,
+    NgIf
   ],
   templateUrl: './directiva.component.html'
 })
 export class DirectivaComponent {
   listaCurso = ['TypeScript', 'JavaScript', 'Java', 'C#', 'PHP'];
+  habilitar = true;
+
+  setHabilitar(){
+    this.habilitar = (this.habilitar != true);
+  }
 }
