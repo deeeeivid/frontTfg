7ac492599fd156f236c0129fcb66a179a0da81e4
Dmzeta <98977215+Dmzeta@users.noreply.github.com>
2024-02-20 20:48:43 +0100

    rutas spa

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,9 +1,14 @@
 import {Component} from "@angular/core";
+import {RouterLink, RouterLinkActive} from "@angular/router";
 
 
 @Component({
     selector: 'app-header',
     templateUrl: './header.component.html',
+  imports: [
+    RouterLink,
+    RouterLinkActive
+  ],
     standalone: true
   }
 )
