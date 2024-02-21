7063516b286321c746d13830338be9133564509d
Dmzeta <98977215+Dmzeta@users.noreply.github.com>
2024-02-21 22:55:39 +0100

    conectando front con back

--- src/app/clientes/cliente.service.ts
+++ src/app/clientes/cliente.service.ts
@@ -1,18 +1,22 @@
 import {Injectable} from '@angular/core';
 import {Cliente} from "./cliente";
-import {CLIENTES} from "./clientes.json";
-import {Observable, of} from "rxjs";
+import {Observable} from "rxjs";
+import {HttpClient} from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ClienteService {
 
-  constructor() {
+  private urlEndpoint = 'http://localhost:8080/api/clientes';
+
+  constructor(
+    private http: HttpClient
+  ) {
 
   }
 
   getClientes(): Observable<Cliente[]> {
-    return of(CLIENTES);
+    return this.http.get<Cliente[]>(this.urlEndpoint)
   }
 }
