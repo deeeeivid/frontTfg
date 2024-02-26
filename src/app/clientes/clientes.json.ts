583e65a818f49fe23510b8f4836f98b0ccc179c3
Dmzeta <98977215+Dmzeta@users.noreply.github.com>
2024-02-26 21:59:26 +0100

    mejora overflow en listado de clientes

--- src/app/clientes/clientes.json.ts
+++
@@ -1,9 +0,0 @@
-import {Cliente} from "./cliente";
-
-export const CLIENTES: Cliente[] = [
-  {id: 1, nombre: 'David', apellido: 'Muñoz', email: 'david@hotmail.com', createAt: '2024-02-14'},
-  {id: 2, nombre: 'Ivan', apellido: 'Martin', email: 'ivan@hotmail.com', createAt: '2024-02-15'},
-  {id: 3, nombre: 'Daniel', apellido: 'Lopez', email: 'daniel@hotmail.com', createAt: '2024-02-15'},
-  {id: 4, nombre: 'Victor', apellido: 'Matias', email: 'victor@hotmail.com', createAt: '2024-02-16'},
-  {id: 5, nombre: 'Sergio', apellido: 'Martin', email: 'sergio@hotmail.com', createAt: '2024-02-16'}
-];
