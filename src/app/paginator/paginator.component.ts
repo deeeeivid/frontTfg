2ca7fc487925b585707d85bb41cd1985713fcbbd
Dmzeta <98977215+Dmzeta@users.noreply.github.com>
2024-03-12 23:45:31 +0100

    rangos de paginacion, onchanges necesario para cuando cambia la pagina

--- src/app/paginator/paginator.component.ts
+++ src/app/paginator/paginator.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input, OnChanges, OnInit, SimpleChanges} from '@angular/core';
 import {NgClass, NgForOf, NgIf} from "@angular/common";
 import {IPagina} from "./paginator.models";
 import {RouterLink} from "@angular/router";
@@ -14,17 +14,36 @@ import {RouterLink} from "@angular/router";
   ],
   templateUrl: './paginator.component.html'
 })
-export class PaginatorComponent implements OnInit {
+export class PaginatorComponent implements OnInit, OnChanges {
 
   @Input() paginador: IPagina;
   paginas: number[];
+
+  desde: number;
+  hasta: number;
 
   constructor() {
   }
 
   ngOnInit(): void {
-    this.paginas = new Array(this.paginador.totalPages).fill(0).map((_valor, indice) => indice + 1);
+    this.initPaginator();
   }
 
-  protected readonly length = length;
+  ngOnChanges(changes: SimpleChanges): void {
+    let paginadorActualizado = changes['paginador'];
+
+    if (paginadorActualizado.previousValue) {
+      this.initPaginator()
+    }
+  }
+
+  private initPaginator(): void {
+    this.desde = Math.min(Math.max(1, this.paginador.number - 4), this.paginador.totalPages - 5);
+    this.hasta = Math.max(Math.min(this.paginador.totalPages, this.paginador.number + 4), 6);
+    if (this.paginador.totalPages > 5) {
+      this.paginas = new Array(this.hasta - this.desde + 1).fill(0).map((_valor, indice) => indice + this.desde);
+    } else {
+      this.paginas = new Array(this.paginador.totalPages).fill(0).map((_valor, indice) => indice + 1);
+    }
+  }
 }
