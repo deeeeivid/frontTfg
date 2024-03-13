2a9661996b6ff807a2955ce76db7c29dfabf1251
Dmzeta <98977215+Dmzeta@users.noreply.github.com>
2024-03-13 22:06:40 +0100

    add angular material

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,8 +4,9 @@ import {routes} from './app.routes';
 import {provideHttpClient} from "@angular/common/http";
 import {registerLocaleData} from "@angular/common";
 import localeES from "@angular/common/locales/es";
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 registerLocaleData(localeES, 'es');
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideHttpClient(), {provide: LOCALE_ID, useValue: 'es'}]
+  providers: [provideRouter(routes), provideHttpClient(), {provide: LOCALE_ID, useValue: 'es'}, provideAnimationsAsync()]
 };
