c3bab96c140afc4e29e39154da4a9f7d6256f820
Dmzeta <98977215+Dmzeta@users.noreply.github.com>
2024-03-19 21:52:55 +0100

    subida del archivo en el front

--- src/app/clientes/detalle/detalle.component.ts
+++ src/app/clientes/detalle/detalle.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {Cliente} from "../cliente";
 import {ClienteService} from "../cliente.service";
 import {ActivatedRoute} from "@angular/router";
+import swal from "sweetalert2";
 
 @Component({
   selector: 'detalle-cliente',
@@ -14,6 +15,8 @@ export class DetalleComponent implements OnInit {
 
   cliente: Cliente;
   titulo: string = "Detalle del cliente";
+
+  private fotoSeleccionada: File;
 
   constructor(
     private clienteService: ClienteService,
@@ -31,4 +34,15 @@ export class DetalleComponent implements OnInit {
     })
   }
 
+  seleccionarFoto(event) {
+    this.fotoSeleccionada = event.target.files[0];
+    console.log(this.fotoSeleccionada);
+  }
+
+  subirFoto() {
+    this.clienteService.subirFoto(this.fotoSeleccionada, this.cliente.id).subscribe(cliente => {
+      this.cliente = cliente;
+      swal.fire('La foto se ha subido correctamente.', `La foto se ha subido con éxito: ${this.cliente.foto}`, 'success');
+    })
+  }
 }
