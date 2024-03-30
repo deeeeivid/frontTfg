7929ecdb0cf37acee28ff6ea25b71bef568199f4
Dmzeta <98977215+Dmzeta@users.noreply.github.com>
2024-03-30 17:33:52 +0100

    nueva clase Usuario y metodo login

--- src/app/usuarios/login.component.ts
+++ src/app/usuarios/login.component.ts
@@ -1,18 +1,33 @@
 import {Component, OnInit} from '@angular/core';
+import {Usuario} from "./usuario";
+import swal from 'sweetalert2';
+import {FormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule
+  ],
   templateUrl: './login.component.html'
 })
 export class LoginComponent implements OnInit {
 
   titulo = 'Iniciar sesión';
+  usuario: Usuario;
 
   constructor() {
+    this.usuario = new Usuario();
   }
 
   ngOnInit() {
+  }
+
+  login() {
+    console.log(this.usuario);
+    if (this.usuario.username == null || this.usuario.password == null){
+      swal.fire('Error Login', 'Username o password vacias!', 'error')
+      return;
+    }
   }
 }
