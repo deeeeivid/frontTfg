bb3829af5d5b765f3be1ce64be44e4b48d949fe9
Dmzeta <98977215+Dmzeta@users.noreply.github.com>
2024-04-09 09:49:48 +0200

    Seguridad Guard en rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,13 +3,14 @@ import {DirectivaComponent} from "./directiva/directiva.component";
 import {ClientesComponent} from "./clientes/clientes.component";
 import {FormComponent} from "./clientes/form.component";
 import {LoginComponent} from "./usuarios/login.component";
+import {AuthGuard} from "./usuarios/guards/auth.guard";
 
 export const routes: Routes = [
   {path: '', redirectTo: '/clientes', pathMatch: 'full'},
   {path: 'directivas', component: DirectivaComponent},
   {path: 'clientes', component: ClientesComponent},
   {path: 'clientes/page/:page', component: ClientesComponent},
-  {path: 'clientes/form', component: FormComponent},
-  {path: 'clientes/form/:id', component: FormComponent},
+  {path: 'clientes/form', component: FormComponent, canActivate: [AuthGuard]},
+  {path: 'clientes/form/:id', component: FormComponent, canActivate: [AuthGuard]},
   {path: 'login', component: LoginComponent},
 ];
---
+++ src/app/usuarios/guards/auth.guard.ts
@@ -0,0 +1,19 @@
+import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree} from '@angular/router';
+import {Observable} from "rxjs";
+import {Injectable} from "@angular/core";
+import {AuthService} from "../auth.service";
+
+@Injectable({providedIn: 'root'})
+export class AuthGuard implements CanActivate {
+
+  constructor(private router: Router, private authService: AuthService) {
+  }
+
+  canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean {
+    if (this.authService.isAuthenticated()) {
+      return true;
+    }
+    this.router.navigate(['/login']);
+    return false;
+  }
+}
