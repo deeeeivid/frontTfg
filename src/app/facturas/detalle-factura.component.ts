db1bfbe5e7862a4c291982b536ba7f17afc51a96
Dmzeta <98977215+Dmzeta@users.noreply.github.com>
2024-04-27 19:10:05 +0200

    añadiendo detalle a la factura

--- src/app/facturas/detalle-factura.component.ts
+++ src/app/facturas/detalle-factura.component.ts
@@ -1,11 +1,32 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FacturaService} from "./services/factura.service";
+import {ActivatedRoute, RouterLink} from "@angular/router";
+import {Factura} from "./models/factura";
+import {NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-detalle-factura',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLink,
+    NgIf
+  ],
   templateUrl: './detalle-factura.component.html'
 })
-export class DetalleFacturaComponent {
+export class DetalleFacturaComponent implements OnInit {
+
+  factura: Factura;
+  titulo = 'Factura';
+
+  constructor(private facturaService: FacturaService, private activatedroute: ActivatedRoute) {
+
+  }
+
+  ngOnInit(): void {
+    this.activatedroute.paramMap.subscribe(params => {
+      let id = +params.get('id');
+      this.facturaService.getFactura(id).subscribe(factura => this.factura = factura);
+    })
+  }
 
 }
