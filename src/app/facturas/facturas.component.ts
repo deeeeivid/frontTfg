7d40ff115390a071479f94cb38e3c0b7c9d51d1f
Dmzeta <98977215+Dmzeta@users.noreply.github.com>
2024-04-29 21:12:12 +0200

    actualizando cantidad de un item

--- src/app/facturas/facturas.component.ts
+++ src/app/facturas/facturas.component.ts
@@ -86,4 +86,14 @@ export class FacturasComponent implements OnInit {
     event.option.focus();
     event.option.deselect();
   }
+
+  actualizarCantidad(id: number, event: any): void {
+    let cantidad = event.target.value as number;
+    this.factura.items = this.factura.items.map((item: ItemFactura) => {
+      if (id === item.producto.id) {
+        item.cantidad = cantidad;
+      }
+      return item;
+    });
+  }
 }
